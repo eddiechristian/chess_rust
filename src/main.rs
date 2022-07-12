62d4e0f2b58da53c833133493ec8b58b85b43068
Eddie.christian <eddiechristian@zoho.com>
2022-07-12 10:23:50 -0400

    ....

--- src/main.rs
+++ src/main.rs
@@ -373,9 +373,9 @@ impl Game {
     }
 }
 fn main() {
-    //  let mut chess_game = Game::game_from_turn_history(&["a2-a4","b7-b5","a4-b5","f7-f5","b5-b6","b8-c6",
-    //      "b6-b7","f5-f4","a1-a7","g7-g6","d2-d4","h7-h5","d4-d5","h5-h4", "b2-b4","c6-a5", "b4-b5","c7-c5"]);  
-    let mut chess_game = Game::default();   
+     let mut chess_game = Game::game_from_turn_history(&["a2-a4","b7-b5","a4-b5","f7-f5","b5-b6","b8-c6",
+         "b6-b7","f5-f4","a1-a7","g7-g6","d2-d4","h7-h5","d4-d5","h5-h4", "b2-b4","c6-a5", "b4-b5","c7-c5"]);  
+    //let mut chess_game = Game::default();   
     chess_game.get_validated_moves(chess_game.state.player_turn);
     let mut game_over = false;
     while game_over == false {
